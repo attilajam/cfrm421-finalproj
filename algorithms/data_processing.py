4935dd43e0374ba8840174a86c567151d569217b
Attila Jamilov <attila.jamilov@gmail.com>
2025-05-31 14:56:37 -0700

    shared file

---
+++ algorithms/data_processing.py
@@ -0,0 +1,8 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv("./train.csv")
+y = df["target"]
+features = [col for col in df.columns if col in ["target", "near_price", "far_price"]]
+X = df[features]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
